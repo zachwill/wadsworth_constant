1eb8bb44ea80a08181ee8d899cbf54fd95b68b61
Zach Williams <hey@zachwill.com>
2011-10-05 00:17:52 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Wadsworth Constant
+==================
+
+For more information, visit the [following Reddit link](http://redd.it/kxtzp).
+
+```bash
+    $ python wadsworth.py "Perhaps this should be known as the Wadsworth Constant"
+    be known as the Wadsworth Constant
+
+    $ echo "Perhaps this shoudl be known as the Wadsworth Constant" | python wadsworth.py
+    be known as the Wadsworth Constant
+```
---
+++ wadsworth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Wadsworth Constant: http://redd.it/kxtzp
+
+This script can apply the Wadsworth Constant to any input -- whether that input
+be from `sys.stdin` or `sys.argv`.
+"""
+
+import sys
+
+
+def wadsworth_constant(phrase=None):
+    """
+    The Wadsworth Constant states the first 30% of any input can be
+    removed, yet the overall message remains the same.
+    """
+    if phrase:
+        phrase = phrase.strip()
+    elif len(sys.argv) > 1:
+        phrase = ' '.join(sys.argv[1:])
+    else:
+        phrase = sys.stdin.readline().strip()
+    index = int(round(len(phrase) * .3))
+    first_word = phrase.find(' ', index) + 1
+    wadsworth_value = phrase[first_word:]
+    return wadsworth_value
+
+
+if __name__ == '__main__':
+    print wadsworth_constant()
